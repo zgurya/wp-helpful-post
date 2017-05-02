d294d24f3c6b7a55f8d86c90b9a0d5b1927581f4
zgurya <a.zgurya@gmail.com>
2017-05-02 18:57:01 +0300

    Some updates

--- wp-helpful-post.php
+++ wp-helpful-post.php
@@ -1,16 +1,15 @@
 <?php
 /**
-* Plugin Name: WordPress Helpful Post
-* Plugin URI: https://github.com/zgurya/wp-helpful-post
-* Description: This plugin allows you to understand which post is most helpful.
-* Version: 0.1
-* Author: a.zgurya
-* Author URI: https://www.facebook.com/a.zgurya
-*/
+ * Plugin Name: WordPress Helpful Post
+ * Plugin URI: https://github.com/zgurya/wp-helpful-post
+ * Description: This plugin allows you to understand which post is most helpful.
+ * Version: 0.1
+ * Author: a.zgurya
+ * Author URI: https://www.facebook.com/a.zgurya
+ */
 
 add_action('admin_menu', 'wp_helpful_post_menu');
 add_action('wp_enqueue_scripts', 'wp_helpful_post_scripts');
-register_activation_hook(__FILE__, 'wp_helpful_post_activation');
 register_uninstall_hook(__FILE__, 'wp_helpful_post_uninstall');
 
 function wp_helpful_post_menu(){
@@ -28,18 +27,18 @@ function wp_helpful_post_scripts(){
 	wp_localize_script('wp-helpful-post-front-js', 'ajax_object',array( 'ajaxurl' => admin_url( 'admin-ajax.php' )));
 }
 
-function wp_helpful_post_activation(){
-	if(!get_option('wp_helpful_post')) add_option('wp_helpful_post');
-}
-
 function wp_helpful_post_uninstall(){
-	delete_option('wp_helpful_post');
+	global $wpdb;
+	$table_name = $wpdb->prefix . "postmeta";
+	$query='DELETE FROM '.$table_name.' WHERE wp_postmeta="post_rating_no" OR wp_postmeta="post_rating_yes"';
+	require_once ABSPATH.'wp-admin/includes/upgrade.php';
+	dbDelta($query);
 }
 
 add_shortcode( 'wp_helpful_post', 'wp_helpful_post_init' );
 function wp_helpful_post_init(){
 	global $post;
-	$output='<div class="wp-helpful-post" data-postid="'.$post->ID.'"><div class="question">Статья была полезной?</div><div class="answers"><span class="yes" data-helpful="yes">Да</span><span class="no" data-helpful="no">Нет</span></div><div class="results"><span></span> Пользователей считают статью полезной</div></div>';
+	$output='<div class="wp-helpful-post" data-postid="'.$post->ID.'"><div class="question">Статья была полезной?</div><div class="answers"><span class="yes" data-helpful="yes">Да</span><span class="no" data-helpful="no">Нет</span></div><div class="results"><span>'.get_wp_helpful_post().'</span> Пользователей считают статью полезной</div></div>';
 	return $output;
 }
 
@@ -67,31 +66,27 @@ function set_wp_helpful_post_ajax(){
 	die();
 }
 
-add_action( 'wp_ajax_get_wp_helpful_post', 'get_wp_helpful_post_ajax' );
-add_action( 'wp_ajax_nopriv_get_wp_helpful_post','get_wp_helpful_post_ajax' );
-function get_wp_helpful_post_ajax(){
-	if(isset($_POST['postid']) && !empty($_POST['postid'])){
-		echo get_wp_helpful_post();
-	}
-	die();
-}
-
 function get_wp_helpful_post(){
-	if(get_post_meta( $_POST['postid'], 'post_rating_yes', true)){
-		$yes=get_post_meta( $_POST['postid'], 'post_rating_yes', true);
+	global $post;
+	if(isset($post)&&!empty($post)){
+		if(get_post_meta( $post->ID, 'post_rating_yes', true)){
+			$yes=get_post_meta( $post->ID, 'post_rating_yes', true);
+		}else{
+			$yes=0;
+		}
+		if(get_post_meta( $post->ID, 'post_rating_no', true)){
+			$no=get_post_meta( $post->ID, 'post_rating_no', true);
+		}else{
+			$no=0;
+		}
+		if($yes>0 && $no==0) return '100%';
+		if(($no>0 && $yes==0) || $no>$yes || ($yes==0 && $no==0)) return '0%';
+		if($yes>0 && $no>0 && $yes>=$no){
+			return ceil(($yes/($yes+$no))*100).'%';
+		}
 	}else{
-		$yes=0;
+		return '0%';
 	}
-	if(get_post_meta( $_POST['postid'], 'post_rating_no', true)){
-		$no=get_post_meta( $_POST['postid'], 'post_rating_no', true);
-	}else{
-		$no=0;
-	}
-	if($yes>0 && $no==0) return '100%';
-	if(($no>0 && $yes==0) || $no>$yes) return '0%';
-	if($yes>0 && $no>0 && $yes>=$no){
-		return ceil(($yes/($yes+$no))*100).'%';
-	}
+	
 }
-
-
+?>
